9ce09d022a8f8bb61d7f0a504d12da2a7f8ea8ba
SadikRa <sadikrahman494@gmail.com>
2024-12-21 22:13:10 +0600

    server run done

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+// app.use('/api/v1', router);
+
+const test = (req: Request, res: Response) => {
+  res.send('hello Sadik');
+};
+
+app.get('/', test);
+
+// app.use(globalErrorHandler);
+
+//Not Found
+// app.use(notFound);
+
+export default app;
