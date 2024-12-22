94e43403861c1049a03b0eb06535ce3bd24ed0b5
SadikRa <sadikrahman494@gmail.com>
2024-12-22 18:31:16 +0600

    interface created done

---
+++ src/app/modules/blog/blog.interface.ts
@@ -0,0 +1,10 @@
+import { ObjectId } from "mongoose";
+
+export interface IBlog {
+  title: string; 
+  content: string; 
+  author: ObjectId;
+  isPublished: boolean; 
+  createdAt: Date; 
+  updatedAt: Date;
+}
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    role: "admin" | "user";
+    isBlocked: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
